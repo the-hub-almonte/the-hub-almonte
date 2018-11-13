9e75d8be2d9c9b2b2f081748fe682a585eeada0e
arobillard <robi0653@algonquinlive.com>
2018-11-12 22:52:30 -0500

    Fix spelling mistake and add sticky to categories

---
+++ js/about.js
@@ -0,0 +1,19 @@
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {subNavSticky()};
+
+// Get the navbar
+var aboutNavStick = document.getElementById("about-subnav-bar");
+
+// Get the offset position of the navbar
+var aboutNavSticky = aboutNavStick.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function subNavSticky() {
+  if (window.pageYOffset >= aboutNavSticky) {
+    aboutNavStick.classList.add("sticky")
+    $('.masthead').addClass('no-shadow');
+  } else {
+    aboutNavStick.classList.remove("sticky");
+    $('.masthead').removeClass('no-shadow');
+  }
+}
---
+++ js/news.js
@@ -0,0 +1,17 @@
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {categoriesSticky()};
+
+// Get the navbar
+var catStick = document.getElementById("news-categories");
+
+// Get the offset position of the navbar
+var catSticky = catStick.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function categoriesSticky() {
+  if (window.pageYOffset >= catSticky) {
+    catStick.classList.add("sticky")
+  } else {
+    catStick.classList.remove("sticky");
+  }
+}
